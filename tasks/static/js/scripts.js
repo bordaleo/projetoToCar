44b0c9e706b5bb81c7427078612c1fef75f28e77
Leo Borda <lsouzaborda@gmail.com>
2025-10-07 11:10:34 -0300

    Site Django

---
+++ tasks/static/js/scripts.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+    var baseUrl = "http://localhost:8000/";
+    var deleteBtn = $('.delete-btn');
+    var searchBtn = $('#search-btn');
+    var searchForm = $('#search-form');
+    var filter     = $('#filter');
+
+    $(deleteBtn).on('click', function(e){
+
+        e.preventDefault();
+
+        var delLink = $(this).attr('href');
+        var result = confirm ('Quer deletar essa tarefa?');
+
+        if(result){
+            window.location.href = delLink;
+        }
+    });
+
+    $(searchBtn).on('click', function (){
+        searchForm.submit();
+
+    });
+
+    $(filter).change(function(){
+        var filter = $(this).val();
+        console.log(filter);
+        window.location.href = baseUrl + '?filter=' + filter;
+    });
+});
+
---
+++ tasks/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.taskList, name='task-list'),
+    path('task/<int:id>/', views.taskView, name='task-view'),
+    path('newtask/', views.newTask, name='task-new'),
+    path('edit/<int:id>/', views.editTask, name='task-edit'),
+    path('changestatus/<int:id>/', views.changeStatus, name='change-status'),
+    path('delete/<int:id>/', views.deleteTask, name='task-delete'),
+    path('hello/', views.helloWorld, name='hello'),
+    path('yourname/<str:name>/', views.yourName, name='yourname'),
+    path('sobre/', views.sobre, name='sobre'),
+
+]
+
